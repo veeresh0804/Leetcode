066060a67f71e69dcf7326d7ecdee5b5a21b5e60
DHANUSH SIVA SAI MANOHARA VEERESH VENNA <manoharaveeresh0804@gmail.com>
2026-07-28 10:48:51 +0530

    Time: 1 ms (45.89%), Space: 46.9 MB (88.73%) - LeetHub

---
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -0,0 +1,20 @@
+class Solution {
+        int diameter = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+         height(root);
+        return diameter;
+    }
+     private int height(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+
+        int leftHeight = height(node.left);
+        int rightHeight = height(node.right);
+
+        diameter = Math.max(diameter, leftHeight + rightHeight);
+
+        return 1 + Math.max(leftHeight, rightHeight);
+    }
+}
